6ad8102aefe44b3099f7240c9ee4073ac5be0e5f
Janhvi <janhvibhati019@gmail.com>
2026-08-14 18:23:45 +0530

    adding codechef solutions

---
+++ 686_X_jumps.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y;
+        z=((x/y)+(x%y));
+        cout<<z<<endl;
+         }
+return 0;
+}
