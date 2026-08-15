680a22a558b6bbf5773c5c2dfc1a2925230ce75e
Janhvi <janhvibhati019@gmail.com>
2026-08-15 21:18:53 +0530

    adding codechef solutions

---
+++ 729_valentine_gift.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+      int t;
+      cin>>t;
+      while(t--){
+          int x;
+          cin>>x;
+          if(x>=127){
+              cout<<"yes"<<endl;
+          }
+          else{
+              cout<<"no"<<endl;
+          }
+      }
+return 0;
+}
