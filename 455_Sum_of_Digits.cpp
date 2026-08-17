bda348551393f553b87eac3795938204fd310aa3
Janhvi <janhvibhati019@gmail.com>
2026-08-17 17:57:48 +0530

    adding codechef solutions

--- 455_Sum_of_Digits.cpp
+++ 455_Sum_of_Digits.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int T;
     cin >> T;
-
     while (T--)
     {
         int N;
         cin >> N;
-
         int sum = 0;
-
         while (N != 0)
         {
             int digit = N % 10;
             sum = sum + digit;
             N = N / 10;
         }
-
-        cout << sum << endl;
+          cout << sum << endl;
     }
 
     return 0;
