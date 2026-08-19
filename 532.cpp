acd08427ce48ed9b7726dabdd1bb0c2cdd35afce
Janhvi <janhvibhati019@gmail.com>
2026-08-19 20:51:39 +0530

    adding codechef solutions

---
+++ 532.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y;
+	    z=4*x+y;
+	    cout<<z<<endl;
+	}
+
+ return 0;
+}
