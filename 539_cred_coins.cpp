03a7597c0096de227f3d6cadc5654b64ee1b1069
Janhvi <janhvibhati019@gmail.com>
2026-08-20 15:52:02 +0530

    adding codechef solutions

---
+++ 539_cred_coins.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    int bag;
+	    cin>>x>>y;
+	    z=x*y;
+	    bag=z/100;
+	    cout<<bag<<endl;
+	}
+	return 0;
+}
