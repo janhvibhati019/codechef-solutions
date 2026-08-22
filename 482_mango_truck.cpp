9fc20057e54d91ea777a58dcb8fca873d414af8f
Janhvi <janhvibhati019@gmail.com>
2026-08-22 10:39:47 +0530

    adding odechef solutions

---
+++ 482_mango_truck.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+     int x,y,z;
+     cin>>x>>y>>z;
+     int n=0;
+     n=((z-y)/x);
+    cout<<n<<endl;
+    }
+    return 0;
+}
