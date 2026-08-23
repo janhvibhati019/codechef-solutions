47c1a4645d38ed7779d8e947d42cc97227e451b1
Janhvi <janhvibhati019@gmail.com>
2026-08-23 21:12:03 +0530

    adding codechef solutions

---
+++ 563_sugracane.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int z=n*50;
+	    int p;
+	    p=z*0.3;
+	    cout<<p<<endl;
+	}
+	return 0;
+}
