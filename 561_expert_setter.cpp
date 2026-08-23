562a3dc4fc8d741bb86b337114fd5522dfbb6f95
Janhvi <janhvibhati019@gmail.com>
2026-08-23 21:00:43 +0530

    adding codechef solutions

---
+++ 561_expert_setter.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(2*y>=x){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+return 0;
+}
