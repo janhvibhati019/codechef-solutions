f88534177ba46da952083d3d1a372f2ef2c18917
Janhvi <janhvibhati019@gmail.com>
2026-08-23 20:36:32 +0530

    adding codechef solutions

---
+++ 541_sale_season.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<=100){
+	        cout<<x<<endl;
+	    }
+	    else if(x<=1000 && x>100){
+	    cout<<x-25<<endl;    
+	    }	
+	    else if(x<=5000 && x>1000){
+	        cout<<x-100<<endl;
+	    }
+	    else if(x>5000){
+	        cout<<x-500<<endl;
+	    }
+	    
+	}
+return 0;
+}
