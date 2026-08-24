0e86d912638485ebad5f6f6b32dd322b7ac94e19
Janhvi <janhvibhati019@gmail.com>
2026-08-24 14:16:12 +0530

    adding codechef solutions

---
+++ 572_score.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+         int t;
+         cin>>t;
+         while(t--){
+            int a,b;
+            cin>>a>>b;
+            int c,d;
+            cin>>c>>d;
+            if(c>=a && d>=b){
+                cout<<"possible"<<endl;
+            }
+            else{
+                cout<<"impossible"<<endl;
+            }
+         }
+         return 0;
+}
