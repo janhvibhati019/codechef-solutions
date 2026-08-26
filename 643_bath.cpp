223fd026bea5341f9a797b2bc34a834014b8be8a
Janhvi <janhvibhati019@gmail.com>
2026-08-26 16:08:44 +0530

    adding codechef solutions

---
+++ 643_bath.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int z=x/(2*y);
+	    cout<<z<<endl;
+	}
+return 0;
+}
