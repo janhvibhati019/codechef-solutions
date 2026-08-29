9731a8f075d7f6ac320e5d832ce6b8ae7a1ee9ea
Janhvi <janhvibhati019@gmail.com>
2026-08-29 20:19:10 +0530

    adding codechef solutions

---
+++ 622_throw.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int z;
+	    z=max({a,b,c});
+	    cout<<z<<endl;
+	}
+return 0;
+}
