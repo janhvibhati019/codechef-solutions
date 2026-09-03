7c6646a3d3e2f5aadf03a7026aefbb9dfc44867f
Janhvi <janhvibhati019@gmail.com>
2026-09-03 14:20:56 +0530

    adding codechef solutions

---
+++ 706_eren.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        if(n%2==0){
+            cout<<(n/2)*(a+b)<<endl;
+        }
+        else{
+            cout<<(n/2)*(a+b)+b<<endl;
+        }
+    }
+return 0;
+}
