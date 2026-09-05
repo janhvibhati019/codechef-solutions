18763a88fc6c1b409ce38a5289d2243d74a8538a
Janhvi <janhvibhati019@gmail.com>
2026-09-05 12:47:18 +0530

    adding codechef solutions

---
+++ 639_police.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int z=abs(x-y);
+	    cout<<z<<endl;
+	}
+return 0;
+}
