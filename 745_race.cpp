1f84e4dc68614c1af409ce06aac9e4465a98590a
Janhvi <janhvibhati019@gmail.com>
2026-09-07 09:28:36 +0530

    adding codechef solutions

---
+++ 745_race.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,a,b;
+	    cin>>x>>y>>a>>b;
+	    int count=0;
+	   if(x != a && x != b){
+	   count++;
+	   }
+	   if(y != a && y != b){
+	   count++;
+	   }
+	   cout<<count<<endl;
+	}
+return 0;
+}
