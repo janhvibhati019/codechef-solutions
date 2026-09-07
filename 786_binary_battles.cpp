e55e6af18c9e04f11bab995172a629dc0de69fd2
Janhvi <janhvibhati019@gmail.com>
2026-09-07 20:42:56 +0530

    adding codechef solutions

---
+++ 786_binary_battles.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,a,b;
+	    cin>>n>>a>>b;
+	    int r=log2(n);
+	    int duration=r*a;
+	    int breakk=(r-1)*b;
+	    int final=duration+breakk;
+	    cout<<final<<endl;
+	}
+	return 0;
+}
